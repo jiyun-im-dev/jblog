ae32672a2461aa8c2fb912af5759c6c7088a2893
jiyun.im.dev <jiyun.im.dev@gmail.com>
2023-07-17 22:51:13 +0900

    댓글 등록 기능을 구현하는 서비스 클래스 추가

---
+++ src/main/java/com/yun/JBlogWeb/service/ReplyService.java
@@ -0,0 +1,30 @@
+package com.yun.JBlogWeb.service;
+
+import com.yun.JBlogWeb.domain.Post;
+import com.yun.JBlogWeb.domain.Reply;
+import com.yun.JBlogWeb.domain.User;
+import com.yun.JBlogWeb.persistence.PostRepository;
+import com.yun.JBlogWeb.persistence.ReplyRepository;
+import com.yun.JBlogWeb.persistence.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ReplyService {
+
+	@Autowired
+	PostRepository postRepository;
+
+	@Autowired
+	ReplyRepository replyRepository;
+
+	@Transactional
+	public void insertReply(int postId, Reply reply, User user) {
+		Post post = postRepository.findById(postId).get();
+		reply.setUser(user);
+		reply.setPost(post);
+		replyRepository.save(reply);
+	}
+
+}
