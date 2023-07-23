e6438d8d1a10bfa6c2bd986ce34f6265600ebff2
jiyun.im.dev <jiyun.im.dev@gmail.com>
2023-07-23 21:34:02 +0900

    댓글 컨트롤러 클래스에 삭제 요청 매핑 메서드 추가

--- src/main/java/com/yun/JBlogWeb/controller/ReplyController.java
+++ src/main/java/com/yun/JBlogWeb/controller/ReplyController.java
@@ -6,12 +6,8 @@ import com.yun.JBlogWeb.dto.ResponseDto;
 import com.yun.JBlogWeb.service.ReplyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -28,6 +24,12 @@ public class ReplyController {
 		User principal = (User) session.getAttribute("principal");
 		replyService.insertReply(postId, reply, principal);
 		return new ResponseDto<>(HttpStatus.OK.value(), postId + "번 포스트에 댓글이 등록됐습니다.");
+	}
+
+	@DeleteMapping("/reply/{replyId}")
+	public @ResponseBody ResponseDto<?> deleteReply(@PathVariable int replyId) {
+		replyService.deleteReply(replyId);
+		return new ResponseDto<>(HttpStatus.OK.value(), replyId + "번 댓글이 삭제됐습니다.");
 	}
 
 }
