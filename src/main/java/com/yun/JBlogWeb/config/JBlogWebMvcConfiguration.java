e0387e3856d9eec65ad1a0cd59e489ba0c95da4d
jiyun.im.dev <jiyun.im.dev@gmail.com>
2023-07-30 22:04:11 +0900

    ModelMapper 라이브러리 추가

--- src/main/java/com/yun/JBlogWeb/config/JBlogWebMvcConfiguration.java
+++ src/main/java/com/yun/JBlogWeb/config/JBlogWebMvcConfiguration.java
@@ -1,11 +1,18 @@
 package com.yun.JBlogWeb.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class JBlogWebMvcConfiguration implements WebMvcConfigurer {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
