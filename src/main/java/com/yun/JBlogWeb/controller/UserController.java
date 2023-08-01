4f5fbad2e910c40a63b275b57b62c0b9b1023540
jiyun.im.dev <jiyun.im.dev@gmail.com>
2023-08-01 22:10:20 +0900

    User 클래스에 대한 유효성 검사 로직을 컨트롤러에 추가

--- src/main/java/com/yun/JBlogWeb/controller/UserController.java
+++ src/main/java/com/yun/JBlogWeb/controller/UserController.java
@@ -2,20 +2,31 @@ package com.yun.JBlogWeb.controller;
 
 import com.yun.JBlogWeb.domain.User;
 import com.yun.JBlogWeb.dto.ResponseDto;
+import com.yun.JBlogWeb.dto.UserDto;
 import com.yun.JBlogWeb.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class UserController {
 
 	@Autowired
 	private UserService userService;
+
+	@Autowired
+	private ModelMapper modelMapper;
 
 	@GetMapping("/auth/insertUser")
 	public String insertUser() {
@@ -24,7 +35,18 @@ public class UserController {
 
 	// 어떤 타입의 데이터가 반환될지 특정할 수 없으므로 ResponseDto<String>이 아님
 	@PostMapping("/auth/insertUser")
-	public @ResponseBody ResponseDto<?> insertUser(@RequestBody User user) {
+	public @ResponseBody ResponseDto<?> insertUser(@Valid @RequestBody UserDto userDto, BindingResult bindingResult) {
+		// UserDto 객체에 대한 유효성 검사
+		if (bindingResult.hasErrors()) {
+			// 에러가 하나라도 있다면 에러 메시지를 Map에 등록
+			Map<String, String> errorMap = new HashMap<>();
+			for (FieldError error : bindingResult.getFieldErrors()) {
+				errorMap.put(error.getField(), error.getDefaultMessage());
+			}
+			return new ResponseDto<>(HttpStatus.BAD_REQUEST.value(), errorMap);
+		}
+
+		User user = modelMapper.map(userDto, User.class);
 		User findUser = userService.getUser(user.getUsername());
 
 		if (findUser.getUsername() == null) {
