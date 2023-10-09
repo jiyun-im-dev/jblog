aa7e1b40e1d27eeaa0a7cc4cd28d17e4c2f5f836
jiyun.im.dev <jiyun.im.dev@gmail.com>
2023-10-09 22:25:55 +0900

    스프링 시큐리티 환경설정 클래스가 UserDetailsServiceImpl을 사용하도록 수정

--- src/main/java/com/yun/JBlogWeb/config/JBlogWebSecurityConfiguration.java
+++ src/main/java/com/yun/JBlogWeb/config/JBlogWebSecurityConfiguration.java
@@ -1,6 +1,9 @@
 package com.yun.JBlogWeb.config;
 
+import com.yun.JBlogWeb.security.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,6 +11,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class JBlogWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
+
+	// 사용자가 입력한 username으로 User 객체를 검색하고 password를 비교한다
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
