cc9598913eb28abd2793a4914d47979d908e66c7
jiyun.im.dev <jiyun.im.dev@gmail.com>
2023-10-24 21:22:13 +0900

    카카오 로그인용 서비스 클래스를 이용하는 컨트롤러 작성

--- src/main/java/com/yun/JBlogWeb/controller/KakaoLoginController.java
+++ src/main/java/com/yun/JBlogWeb/controller/KakaoLoginController.java
@@ -1,5 +1,7 @@
 package com.yun.JBlogWeb.controller;
 
+import com.yun.JBlogWeb.service.KakaoLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +9,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class KakaoLoginController {
 
+	@Autowired
+	private KakaoLoginService kakaoLoginService;
+
 	@GetMapping("/oauth/kakao")
 	public @ResponseBody String kakaoCallback(String code) {
-		return "카카오 서버로부터 받은 CODE 정보: " + code;
+		// 인증 서버로부터 받은 CODE를 이용해 액세스 토큰 획득
+		String accessToken = kakaoLoginService.getAccessToken(code);
+		// 응답을 콘솔과 브라우저에서 출력
+		System.out.println(accessToken);
+		return accessToken;
 	}
 
 }
