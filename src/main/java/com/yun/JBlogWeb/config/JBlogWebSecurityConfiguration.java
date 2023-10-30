9686d890f2dede91d060d59e8bf6ebad5692d9ff
jiyun.im.dev <jiyun.im.dev@gmail.com>
2023-10-30 23:20:53 +0900

    카카오 회원가입 처리

--- src/main/java/com/yun/JBlogWeb/config/JBlogWebSecurityConfiguration.java
+++ src/main/java/com/yun/JBlogWeb/config/JBlogWebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.yun.JBlogWeb.security.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +19,11 @@ public class JBlogWebSecurityConfiguration extends WebSecurityConfigurerAdapter 
 	// 사용자가 입력한 username으로 사용자 인증하는 객체
 	@Autowired
 	private UserDetailsServiceImpl userDetailsService;
+
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
--- src/main/java/com/yun/JBlogWeb/controller/KakaoLoginController.java
+++ src/main/java/com/yun/JBlogWeb/controller/KakaoLoginController.java
@@ -2,7 +2,13 @@ package com.yun.JBlogWeb.controller;
 
 import com.yun.JBlogWeb.domain.User;
 import com.yun.JBlogWeb.service.KakaoLoginService;
+import com.yun.JBlogWeb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,13 +19,32 @@ public class KakaoLoginController {
 	@Autowired
 	private KakaoLoginService kakaoLoginService;
 
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Value("${kakao.default.password}")
+	private String kakaoPassword;
+
 	@GetMapping("/oauth/kakao")
-	public @ResponseBody User kakaoCallback(String code) {
+	public @ResponseBody String kakaoCallback(String code) {
 		// 1. 인증 서버로부터 받은 CODE를 이용해 액세스 토큰 획득
 		String accessToken = kakaoLoginService.getAccessToken(code);
 		// 2. 액세스 토큰을 이용하여 사용자 정보 획득
-		User userInfo = kakaoLoginService.getUserInfo(accessToken);
-		return userInfo;
+		User kakaoUser = kakaoLoginService.getUserInfo(accessToken);
+		// 3. 기존 회원이 아니면 신규 회원으로 등록
+		User findUser = userService.getUser(kakaoUser.getUsername());
+		if (findUser.getUsername() == null) {
+			userService.insertUser(kakaoUser);
+		}
+		// 4. 카카오로부터 받은 사용자 정보를 기반으로 인증 처리
+		UsernamePasswordAuthenticationToken authenticationToken = // 인증 토큰 생성?
+				new UsernamePasswordAuthenticationToken(kakaoUser.getUsername(), kakaoPassword);
+		Authentication authentication = authenticationManager.authenticate(authenticationToken); // 토큰을 인증?
+		SecurityContextHolder.getContext().setAuthentication(authentication); // 인증 정보로 설정?
+		return "redirect:/";
 	}
 
 }
