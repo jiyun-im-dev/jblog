81acb937a2526cb7b917233a419174c75157c09b
jiyun.im.dev <jiyun.im.dev@gmail.com>
2023-11-03 22:36:09 +0900

    구글 회원가입 설정 - OAuth2User 인터페이스 구현

--- src/main/java/com/yun/JBlogWeb/security/UserDetailsImpl.java
+++ src/main/java/com/yun/JBlogWeb/security/UserDetailsImpl.java
@@ -5,19 +5,42 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 @Getter
 @Setter
-public class UserDetailsImpl implements UserDetails {
+public class UserDetailsImpl implements UserDetails, OAuth2User {
+
+	// 구글에서 조회한 사용자 정보를 담을 컬렉션
+	private Map<String, Object> attributes;
 
 	private static final long serialVersionUID = 1L;
 	private User user; // users 테이블과 매핑된 엔티티
 
 	public UserDetailsImpl(User user) {
 		this.user = user;
+	}
+
+	// OAuth 로그인 시 사용할 생성자
+	public UserDetailsImpl(User user, Map<String, Object> attributes) {
+		this.user = user;
+		this.attributes = attributes;
+	}
+
+	// 구글에서 조회한 사용자 정보가 저장된 컬렉션 반환
+	@Override
+	public Map<String, Object> getAttributes() {
+		return attributes;
+	}
+
+	// 이름은 사용하지 않는 정보이므로 null 반환
+	@Override
+	public String getName() {
+		return null;
 	}
 
 	@Override
